01ba7d6f7111f369c5b25b5df304186971af1081
Johannes Harzenetter <johannes.harzenetter@hs-augsburg.de>
2020-02-13 18:27:18 +0100

    some more tryouts

--- Minimizer/src/Runner.java
+++ Minimizer/src/Runner.java
@@ -5,14 +5,19 @@ public class Runner {
         Minimizer minimizer = new Minimizer();
         minimizer.initialize();
         System.out.println("complete: " + minimizer.getAll_terms_combined());
-        System.out.println(minimizer.compare_combine_Terms().size());
+        //System.out.println(minimizer.compare_combine_Terms(minimizer.getTerms()).size());
         /*for(Term t : a){
             System.out.println(t.getCompleteTerm());
+        }
+        for (Term t : minimizer.compare_combine_Terms_rek(minimizer.getTerms())){
+            System.out.println(t.getCompleteTerm());
         }*/
-        for (Term t : minimizer.compare_combine_Terms()){
+        //List<Term> ayy = minimizer.compare_combine_Terms_rek(minimizer.getTerms());
+
+        System.out.println(minimizer.create_Prime_Terms().size());
+        //System.out.println(minimizer.getPrimeTable().size());
+        for(Term t : minimizer.getPrimeTable()){
             System.out.println(t.getCompleteTerm());
         }
-
-        System.out.println(minimizer.getPrimeTable().size());
     }
 }
